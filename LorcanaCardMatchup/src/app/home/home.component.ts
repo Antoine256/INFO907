013f9f4a34652ccb092cf815a23bb108fb7a2da6
Elyroma <elynebeaudeau@orange.fr>
2025-01-16 14:50:03 +0100

    limite

--- LorcanaCardMatchup/src/app/home/home.component.ts
+++ LorcanaCardMatchup/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {Card} from "../../interfaces/Card";
 import {CommonModule, NgFor, NgForOf, NgIf} from "@angular/common";
 import {SearchBarComponent} from "../search-bar/search-bar.component";
@@ -18,15 +18,20 @@ import {CardService} from "../card.service";
 export class HomeComponent {
 
   card!: Card;
-  remplacmentCards!: Card[];
+  replacementCards!: Card[];
+  limit: number = 4;
 
   constructor(private cardService: CardService) {
   }
 
   updateCard(card: Card): void {
     this.card = card
-    this.remplacmentCards = this.cardService.getSimilarCard(card)
-    console.log(this.remplacmentCards)
+    this.updateSimilarCard(4)
   }
 
+  updateSimilarCard(limit: number | string){
+    if (typeof limit === "string")
+      limit = parseInt(limit)
+    this.replacementCards = this.cardService.getSimilarCard(this.card, limit)
+  }
 }
