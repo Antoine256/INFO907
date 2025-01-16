dd350f4876a816032adf6c5ead83ec0b4f0c6690
Elyroma <elynebeaudeau@orange.fr>
2025-01-16 15:48:15 +0100

    filtres

--- LorcanaCardMatchup/src/app/home/home.component.ts
+++ LorcanaCardMatchup/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {Card} from "../../interfaces/Card";
-import {CommonModule, NgFor, NgForOf, NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {SearchBarComponent} from "../search-bar/search-bar.component";
 import {CardService} from "../card.service";
+import {TYPES} from "../../interfaces/types.enum";
 
 @Component({
   selector: 'app-home',
@@ -16,22 +17,40 @@ import {CardService} from "../card.service";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  readonly TYPES = TYPES
   card!: Card;
   replacementCards!: Card[];
   limit: number = 4;
+  types: string[] = [TYPES.PERSONNAGE.value, TYPES.OBJET.value, TYPES.LIEU.value, TYPES.ACTION.value, TYPES.CHANSON.value];
 
   constructor(private cardService: CardService) {
   }
 
   updateCard(card: Card): void {
     this.card = card
-    this.updateSimilarCard(4)
+    this.updateSimilarCard()
   }
 
-  updateSimilarCard(limit: number | string){
-    if (typeof limit === "string")
-      limit = parseInt(limit)
-    this.replacementCards = this.cardService.getSimilarCard(this.card, limit, ["character"])
+  updateSimilarCard(){
+    this.replacementCards = this.cardService.getSimilarCard(this.card, this.limit, this.types)
+  }
+
+  updateFilter(value: string): void {
+    if(this.types.includes(value)){
+      this.types = this.types.filter(type => type !== value)
+    } else {
+      this.types.push(value)
+    }
+    this.updateSimilarCard()
+  }
+
+  updateLimit(value: number | string){
+    if (typeof value === "string")
+      this.limit = parseInt(value)
+    this.updateSimilarCard()
+  }
+
+  isSelected(value: string): boolean {
+    return this.types.includes(value)
   }
 }
---
+++ LorcanaCardMatchup/src/interfaces/types.enum.ts
@@ -0,0 +1,22 @@
+export const TYPES = {
+  PERSONNAGE: {
+    label: "Personnage",
+    value: "character"
+  },
+  OBJET: {
+    label: "Objet",
+    value: "item"
+  },
+  LIEU: {
+    label: "Lieu",
+    value: "location"
+  },
+  ACTION: {
+    label: "Action",
+    value: "action"
+  },
+  CHANSON: {
+    label: "Chanson",
+    value: "action - song"
+  }
+}
